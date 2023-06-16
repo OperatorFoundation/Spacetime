6af5af25445a7ada3d5afc42f5c763ce03d52a50
consuelita <adelita@operatorfoundation.org>
2023-06-16 17:55:38 -0500

    swift-tools-version: 5.8, macOS v13, & iOS v16

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Spacetime",
-    platforms: [.macOS(.v12),
-                .iOS(.v15)],
+    platforms: [.macOS(.v13),
+                .iOS(.v16)],
     products: [
         .library(
             name: "Spacetime",
